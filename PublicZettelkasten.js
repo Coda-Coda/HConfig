baf6f501bd523c4df622f231b4dd14817390247f
Daniel Britten <daniel.britten@outlook.com>
2021-08-10 14:25:50 +1200

    Initial config

--- PublicZettelkasten.js
+++ PublicZettelkasten.js
@@ -1,6 +1,18 @@
 // Hypothesis Config Helper for Daniel Britten's Public Zettelkasten
 window.hypothesisConfig = function () {
 	return {
-	    "openSidebar": true
+		"showHighlights": true
 	};
 };
+
+var blogMetadata = document.createElement('meta');
+blogMetadata.name="dc.relation.ispartof";
+blogMetadata.content="Public-Zettelkasten-Daniel-Britten-(ORCID-0000-0002-7860-3595)";
+document.head.appendChild(blogMetadata);
+
+function setZettelMetadata(zettelId) {
+	var zettelMetadata = document.createElement('meta');
+	zettelMetadata.name="dc.identifier";
+	zettelMetadata.content=zettelId;
+	document.head.appendChild(zettelMetadata);
+}
